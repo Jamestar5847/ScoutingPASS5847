5bbab2908b0b08a430c51ca499eef22bb3858d6f
Jamestar5847 <122237474+Jamestar5847@users.noreply.github.com>
2024-03-06 16:48:57 -0600

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": " 2024paca ",
+      "defaultValue": " 2024ilpe ",
       "required": "true"
     },
     { "name": "Match Level",
